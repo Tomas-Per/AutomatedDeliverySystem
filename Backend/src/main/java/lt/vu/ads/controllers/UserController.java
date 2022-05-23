703274051ffbb6af13ad616cc08832dd7f39fb59
Tomas <tomasperveneckas9@gmail.com>
2022-05-23 13:04:38 +0300

    Removed CORS annotation

--- Backend/src/main/java/lt/vu/ads/controllers/UserController.java
+++ Backend/src/main/java/lt/vu/ads/controllers/UserController.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class UserController {
     private final UserService userService;
 
